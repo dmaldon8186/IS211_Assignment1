9aefd545d83815da109f12f5886c8fa8ed1f12a2
dmaldon8186 <david.maldonado36@spsmail.cuny.edu>
2023-01-29 20:58:12 -0500

    Updated Assignment 1 Part 2

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,11 +1,10 @@
-
 class Book:
-    def __init__(self, author, title):
-        pass
-    
+    def __init__(self,author="",title=""):
+        self.author=author
+        self.title=title
     def display(self):
-        pass
-
-
-if __name__ == "__main__":
-    pass
+        print(f"'{self.title}, written by {self.author}'")
+obj1 = Book("J.K. Rowling", "Harry Potter and the Goblet of Fire")
+obj2 = Book("Walter Scott", "Ivanhoe: A Romance")
+obj1.display()
+obj2.display()
